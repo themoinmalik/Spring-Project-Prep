be8656bafa68d23365dbb5c6d8beadc2737743ee
Moin Malik <Moin Malik>
2022-12-23 18:19:38 +0530

    changes updated

--- src/main/java/com/javatechie/crud/example/SpringProjectPrep.java
+++ src/main/java/com/javatechie/crud/example/SpringProjectPrep.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootCrudExample2Application {
+public class SpringProjectPrep {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootCrudExample2Application.class, args);
+		SpringApplication.run(SpringProjectPrep.class, args);
 	}
 
 }
