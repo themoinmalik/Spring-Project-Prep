16ff21bc7892743ed65969442c4e1f4d484375a4
Moin Malik <Moin Malik>
2023-02-19 22:17:50 +0530

    19 feb

--- src/main/java/com/javatechie/crud/example/SpringCar.java
+++ src/main/java/com/javatechie/crud/example/SpringCar.java
@@ -2,6 +2,7 @@ package com.javatechie.crud.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
--- src/main/java/com/javatechie/crud/example/SpringProjectPrep.java
+++ src/main/java/com/javatechie/crud/example/SpringProjectPrep.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableCaching
+@EnableScheduling
 public class SpringProjectPrep {
 
 	public static void main(String[] args) {
